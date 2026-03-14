491a579b7af620231b9f980ac99553bf58cf968b
jishi113 <btbtc23048_jishi@banasthali.in>
2026-03-14 18:17:11 +0530

    Initial commit - SmartBus Management System

---
+++ react-frontend/src/App.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import ListBusComponent from './components/ListBusComponent';
+import CreateBusComponent from './components/CreateBusComponent';
+import UpdateBusComponent from './components/UpdateBusComponent';
+import ViewBusComponent from './components/ViewBusComponent';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import SignUp from './entry/SignUp';
+import SignIn from './entry/SignIn';
+
+function App() {
+  return (
+    <div>
+      <Router>
+
+        {/* Navbar */}
+        <nav className="navbar navbar-dark bg-dark">
+          <div className="container mt-4">
+            <span className="navbar-brand mb-0 h1">
+              🚌 Bus Management System
+            </span>
+          </div>
+        </nav>
+
+        <div className="container">
+
+<h1 className="text-center mb-4">
+🚌 SmartBus
+</h1>
+
+          <Switch>
+            <Route path="/" exact component={SignIn}></Route>
+            <Route path="/signup" exact component={SignUp}></Route>
+            <Route path="/bus" exact component={ListBusComponent}></Route>
+            <Route path="/busdetails" component={ListBusComponent}></Route>
+            <Route path="/add-bus/:id" component={CreateBusComponent}></Route>
+            <Route path="/view-bus/:id" component={ViewBusComponent}></Route>
+            <Route path="/update-bus/:id" component={UpdateBusComponent}></Route>
+          </Switch>
+
+        </div>
+
+      </Router>
+    </div>
+  );
+}
+
+export default App;
